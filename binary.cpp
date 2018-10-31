57dd344edbddf4656dd1b322c064754436303fd4
Carlos Guerrero <carlos.guerrero@utec.edu.pe>
2018-10-30 23:35:39 -0500

    Implement constructor, bitOn, bitOff, [], clear, destructor.

--- binary.cpp
+++ binary.cpp
@@ -1,34 +1,52 @@
 #ifndef BOOLEAN_H
 #define BOOLEAN_H
 
+#include <iostream>
+
 class boolean {
     private:
         char* array;
         int size;
-    
+
     public:
         boolean(int size) {
-            // TODO
+          this-> size = size;
+          array = new char[size/8+1];
+          for (size_t i = 0; i < size/8+1; i++) {
+            array[i] = 0;
+          }
         }
-        
+
         void bitOn(int position) {
-            // TODO
+          array[position / 8] |= 1<<(position%8);
         }
-             
+
         void bitOff(int position) {
-            // TODO
+          array[position / 8] &= ~(1<<(position%8));
         }
-             
+
         bool operator[](int position) {
-            // TODO
+          array[position/8] &= 1 << (position%8);
+          if (array[position/8] == 0) {
+            return false;
+          } else return true;
         }
-    
-        void clear() {    
-            // TODO
+
+        void clear() {
+          for (size_t i = 0; i < this->size/8+1; i++) {
+            array[i] = 0;
+          }
         }
-             
+
+        void print() {
+          for (size_t i = 0; i < this->size; i++) {
+            //std::cout << this->[i] << ' ';
+          }
+          std::cout << '\n';
+        }
+
         ~boolean() {
-            // TODO
+          delete[] array;
         }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#include "binary.cpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
     cout << "===========================================================" << endl;
-    cout << "\Binary Array Practice" << endl;
+    cout << "Binary Array Practice" << endl;
     cout << "===========================================================" << endl << endl;
 
-    system("read");
-    return EXIT_SUCCESS;
-}
+    boolean xd(16);
+    xd.bitOn(7);
+    cout<<endl<<xd[7]<<endl;
+    //xd.print();
+    xd.bitOff(7);
+    cout<<endl<<xd[7]<<endl;
+    xd.bitOn(10);
+    xd.bitOn(11);
+    cout<<endl<<xd[10]<<endl;
+    //xd.print();
 
+    return 0;
+}
